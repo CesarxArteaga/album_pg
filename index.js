711fbbf43d6c995a4d53cc977130c150aed26aaa
CesarxArteaga <ctmces94@gmail.com>
2020-04-23 01:45:58 -0500

    post create album

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const db = require('./database');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const cancion_model = require('./models/cancion.model')
 const album_model = require('./models/album.model')
 
 app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 db.connect((err)=>{
     if(err)
@@ -25,5 +31,6 @@ app.get('/cancion/:id', cancion_model.getCancion)
 app.get('/album/:id_album/canciones', cancion_model.getCancionesByAlbumId)
 
 app.get('/albumes', album_model.getAlbumes)
+app.post('/albumes', album_model.createAlbum)
 
 app.listen(process.env.PORT || 3333, () => console.log("runing..."))
--- models/album.model.js
+++ models/album.model.js
@@ -11,6 +11,18 @@ const conn  = {
             }
         })
         
+    },
+
+    createAlbum: (req, res)=>{
+        const {titulo_album} = req.body
+        pool.query('INSERT INTO album (titulo_album) VALUES ($1)',[titulo_album], 
+        (err, result)=>{
+            if(err){
+                console.log(err)
+            }else{
+                res.send('new album ' + titulo_album + ' added')
+            }
+        })
     }
 }
 module.exports = conn
